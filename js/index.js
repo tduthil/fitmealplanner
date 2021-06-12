69e82047736d826500dc4c3cedf99e3145d4eed7
Thierry Duthil <tduthil152@gmail.com>
2021-06-11 23:34:47 -0400

    Added more JS & table

--- js/index.js
+++ js/index.js
@@ -15,12 +15,59 @@ function calcTotalCalories() {
     //Meal Plan will use 60% of total calories entered by user (2 meals per day)
     /* var userEntry = document.getElementById("totalCalories");
     document.write(userEntry); */
-    document.getElementById("mealPlan1").style.backgroundColor = "red";
-    
-     var myDiv = document.getElementById("plan");
-    myDiv.innerHTML = "<p>This is a test<p>";
 
-    return false;
+    //document.getElementById("plan").innerText = "Max Calories For Lunch & Dinner: ";
+
+    var maxCal = document.getElementById("totalCalories").value *.60;
+
+    //document.getElementById("plan").innerHTML = `${maxCal}`;
+
+
+   document.getElementById("mealPlan1").innerHTML = `<p>Max Calories For Lunch & Dinner: ${maxCal}</p><br><table class="table">
+                <thead class="table-primary">
+                    <tr>
+                        <th></th>
+                        <!-- <th>Sunday</th> -->
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <!--                 <th>Saturday</th>
+            -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Lunch</th>
+                        <td>meal1</td>
+                        <td>meal3</td>
+                        <td>meal5</td>
+                        <td>meal7</td>
+                        <td>meal9</td>
+                        <!--         <td>meal11</td>
+                            <td>meal13</td> -->
+                    </tr>
+                    <tr>
+                        <th scope="row">Dinner</th>
+                        <td>meal2</td>
+                        <td>meal4</td>
+                        <td>meal6</td>
+                        <td>meal8</td>
+                        <td>meal10</td>
+                        <!--   <td>meal12</td>
+                            <td>meal14</td> -->
+                    </tr>
+        
+                    </tr>
+                </tbody>
+        
+        
+            </table>
+        </div>`; 
+
+    return false; 
+
 }
 
 function planByMacros() {
